3b006e6141cc6546e46a4e9f1bfebafde11a9480
Srinidhi Chitti <146361035+Srinidhi-Chitti@users.noreply.github.com>
2025-08-01 19:24:44 +0530

    Create Kthelementingrammar.c

---
+++ Kthelementingrammar.c
@@ -0,0 +1,8 @@
+int kthGrammar(int n, int k) {
+    if (n == 1) return 0;
+    int mid = 1 << (n - 2); // same as pow(2, n - 2)
+    if (k <= mid)
+        return kthGrammar(n - 1, k);
+    else
+        return 1 - kthGrammar(n - 1, k - mid);
+}
