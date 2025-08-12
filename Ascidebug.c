3b3db1168f4b21345b7b96cc0f1bf7ffcb8244cb
Srinidhi Chitti <146361035+Srinidhi-Chitti@users.noreply.github.com>
2025-08-12 23:21:36 +0530

    Create Ascidebug.c

---
+++ Ascidebug.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int i = 65;
+    switch(i){
+        case 65:
+        printf("Int 65");
+        break;
+        case A:
+        printf("Char 65");
+        break;
+        default:
+        printf("Bye");
+    }
+    return 0;
+}
+
+//o/p: error 65= A in Asci
